c440e60ba993d11a33cbb44c49553876e0ce2b64
Oleg Lozovik <oldisc3@gmail.com>
2021-06-23 13:57:54 +0300

    refactoring function changeThemeClassBody

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -5,33 +5,22 @@ const Theme = {
 
 const inputSwitchEl = document.querySelector('#theme-switch-toggle');
 
-if (localStorage.getItem('theme') === 'dark') {
+if (localStorage.getItem('theme') === Theme.DARK) {
   document.body.classList.add(Theme.DARK);
   inputSwitchEl.checked = true;
 }
 
-const changeThemeClassBody = () => {
-  if (document.body.classList.contains(Theme.LIGHT)) {
-    document.body.classList.remove(Theme.LIGHT);
-    document.body.classList.add(Theme.DARK);
-    localStorage.setItem('theme', 'dark');
-    return;
-  }
-  if (document.body.classList.contains(Theme.DARK)) {
-    document.body.classList.remove(Theme.DARK);
-    document.body.classList.add(Theme.LIGHT);
-    localStorage.setItem('theme', 'light');
-    return;
-  }
-  document.body.classList.add(Theme.DARK);
-  localStorage.setItem('theme', 'dark');
+const changeThemeClassBody = (addClass, removeClass) => {
+  document.body.classList.remove(removeClass);
+  document.body.classList.add(addClass);
+  localStorage.setItem('theme', addClass);
 };
 
 const changeInputHandler = event => {
   if (inputSwitchEl.checked) {
-    changeThemeClassBody();
+    changeThemeClassBody(Theme.DARK, Theme.LIGHT);
   } else {
-    changeThemeClassBody();
+    changeThemeClassBody(Theme.LIGHT, Theme.DARK);
   }
 };
 
